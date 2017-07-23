94c7f90885025993b7892423f40882165b687da1
bersling <bersling@gmail.com>
2017-07-23 17:03:01 +0200

    remove yarn dep, update package.json and add package-lock.json

--- dist/dbadapter.d.ts
+++ dist/dbadapter.d.ts
@@ -1,2 +1,3 @@
+import * as tsmongo from '@tsmean/mongo';
 import * as tsmysql from '@tsmean/mysql';
-export declare function dbadapter(databaseType: 'mongo' | 'mysql'): typeof tsmysql;
+export declare function dbadapter(databaseType: 'mongo' | 'mysql'): typeof tsmongo | typeof tsmysql;
